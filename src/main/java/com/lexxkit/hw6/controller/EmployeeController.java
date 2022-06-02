7a88e321eb2caabaf481518ef8aaee9814692e28
Lexxkit <lexxkit@gmail.com>
2022-06-02 12:22:23 +0300

    created EmployeeController with 4 methods:index, add, remove and find employee;

---
+++ src/main/java/com/lexxkit/hw6/controller/EmployeeController.java
@@ -0,0 +1,45 @@
+package com.lexxkit.hw6.controller;
+
+import com.lexxkit.hw6.data.Employee;
+import com.lexxkit.hw6.service.EmployeeService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/employee")
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    String indexPage() {
+        return "Controller is working!";
+    }
+
+    @GetMapping(path = "/add")
+    Employee addEmployee(@RequestParam("firstName")String firstName,
+                         @RequestParam("lastName") String lastName) {
+
+        return employeeService.addEmployee(firstName, lastName);
+    }
+
+    @GetMapping(path = "/remove")
+    Employee removeEmployee(@RequestParam("firstName")String firstName,
+                            @RequestParam("lastName") String lastName) {
+
+        return employeeService.removeEmployee(firstName, lastName);
+    }
+
+    @GetMapping(path = "/find")
+    Employee findEmployee(@RequestParam("firstName")String firstName,
+                          @RequestParam("lastName") String lastName) {
+
+        return employeeService.findEmployee(firstName, lastName);
+    }
+}
+
